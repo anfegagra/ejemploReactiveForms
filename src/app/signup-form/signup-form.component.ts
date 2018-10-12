94a89b5d651827ac054306548ff2c4d7b53f300b
Andrés Felipe García Granados - Ceiba Software <andres.granados@ceiba.com.co>
2018-10-12 15:47:24 -0500

    rective form con validador personalizado

---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent implements OnInit {
+
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace
+    ]),
+    password: new FormControl('', Validators.required)
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  get username() {
+    return this.form.get('username');
+  }
+
+}
